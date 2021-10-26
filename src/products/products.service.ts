0fe2325584811f3ae7f1f60fad0c949f135a1a2b
SabitTheBot <iamsabit99@gmail.com>
2021-10-26 16:44:19 +0600

    create product done

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,16 +1,26 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose';
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
   private products: Product[] = [];
 
-  insertProduct(title: string, desc: string, price: number) {
-    const prodId = Math.random().toString();
-    const newProduct = new Product(prodId, title, desc, price);
-    this.products.push(newProduct);
-    return prodId;
+  constructor(@InjectModel('Product') private readonly productModel: Model<Product>) { }
+
+  async insertProduct(title: string, desc: string, price: number) {
+
+    const newProduct = new this.productModel({
+      title: title,
+      description: desc,
+      price: price
+    });
+    const result = await newProduct.save();
+
+    console.log("Data Saved");
+
+    return "Data Saved"
   }
 
   getProducts() {
@@ -38,8 +48,8 @@ export class ProductsService {
   }
 
   deleteProduct(prodId: string) {
-      const index = this.findProduct(prodId)[1];
-      this.products.splice(index, 1);
+    const index = this.findProduct(prodId)[1];
+    this.products.splice(index, 1);
   }
 
   private findProduct(id: string): [Product, number] {
